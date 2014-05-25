0c9c8df2888f7ec42306d08d72696f853e35d852
Billy Monks <williamtmonks@gmail.com>
2014-05-25 15:46:10 -0400

    Placer tool basics
    (Bare basics) (Bare bare bare basics)

---
+++ BoostDragoonAdventure/BoostDragoonAdventure/editor/PlacerTool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wickedcrush.editor
+{
+    public enum EditorMode
+    {
+        Layer,
+        Entity
+    }
+
+    public class PlacerTool
+    {
+        EditorMode mode;
+        LayerType layerType;
+        EditorEntity entity;
+
+        public PlacerTool()
+        {
+            mode = EditorMode.Layer;
+            layerType = LayerType.WALL;
+            entity = null;
+        }
+    }
+}
