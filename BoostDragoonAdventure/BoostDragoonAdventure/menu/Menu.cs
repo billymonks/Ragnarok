2c3e2db36a7669bf7e746eea68035ac26fa94613
Billy Monks <williamtmonks@gmail.com>
2014-05-28 22:03:02 -0400

    Menu stuff
    happy birthday 2 me

---
+++ BoostDragoonAdventure/BoostDragoonAdventure/menu/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wickedcrush.menu
+{
+    public class Menu
+    {
+        MenuElement current;
+
+        public Menu()
+        {
+
+        }
+    }
+}
---
+++ BoostDragoonAdventure/BoostDragoonAdventure/menu/MenuElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wickedcrush.menu
+{
+    public class MenuElement
+    {
+        MenuElement left = null, right = null, up = null, down = null;
+        String text;
+        Object value;
+
+        public MenuElement()
+        {
+
+        }
+    }
+}
