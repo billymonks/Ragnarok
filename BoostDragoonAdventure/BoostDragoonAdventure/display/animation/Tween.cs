869eafc8744d41f626d90fcb0d9f96178a8e3bd7
Billy Monks <williamtmonks@gmail.com>
2014-07-17 23:29:13 -0400

    Tween
    tweenin

---
+++ BoostDragoonAdventure/BoostDragoonAdventure/display/animation/Tween.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace wickedcrush.display.animation
+{
+    public class Tween
+    {
+        private TimeSpan elapsed, duration;
+        private float start, end;
+
+        private bool finished;
+
+        public Tween(float start, float end, TimeSpan duration)
+        {
+            this.elapsed = new TimeSpan();
+            this.duration = duration;
+            this.start = start;
+            this.end = end;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            elapsed += gameTime.ElapsedGameTime;
+            
+            if (elapsed >= duration)
+                finished = true;
+        }
+
+        public float getValue()
+        {
+            return start + ( (end - start) * ((float)elapsed.TotalSeconds / (float)duration.TotalSeconds) );
+        }
+    }
+}
