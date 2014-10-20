cf81fbbbb5aa0733221e35b93615f1eb1ec27c74
Billy Monks <williamtmonks@gmail.com>
2014-10-20 12:36:14 -0400

    network manager start
    what am i doing? oh wait i'm doing me

---
+++ BoostDragoonAdventure/BoostDragoonAdventure/manager/network/NetworkManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ExitGames.Client.Photon;
+
+namespace wickedcrush.manager.network
+{
+    public class NetworkManager
+    {
+        PeerListener listener;
+
+        public NetworkManager()
+        {
+            listener = new PeerListener();
+
+            listener.Connect();
+        }
+
+        public void RequestMap(int difficulty, int tier)
+        {
+
+        }
+    }
+}
---
+++ BoostDragoonAdventure/BoostDragoonAdventure/manager/network/PeerListener.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ExitGames.Client.Photon;
+
+namespace wickedcrush.manager.network
+{
+    
+
+    public class PeerListener : IPhotonPeerListener
+    {
+        private int retries = 0, maxRetries = 5;
+        
+        private string serverAddress = "54.172.128.30:4530", serverName = "WorldServer";
+
+        bool connected = false;
+
+        PhotonPeer peer;
+
+        public PeerListener()
+        {
+            peer = new PhotonPeer(this, ConnectionProtocol.Tcp);
+        }
+
+        
+
+        public void  DebugReturn(DebugLevel level, string message)
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        public void  OnEvent(EventData eventData)
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        public void  OnOperationResponse(OperationResponse operationResponse)
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        public void  OnStatusChanged(StatusCode statusCode)
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        public void Connect()
+        {
+            do
+            {
+                connected = peer.Connect(serverAddress, serverName);
+                retries++;
+            } while (!connected && retries < maxRetries);
+        }
+    }
+}
